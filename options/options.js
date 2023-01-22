b569a6c6966fdf7f5f66f93fba2cbe159aa27e16
Crowdbotics <engineering@crowdbotics.com>
2023-01-22 00:18:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-cell-dev-58522",
+  url: "https://late-cell-dev-58522.botics.co",
+  api: "https://late-cell-dev-58522.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
